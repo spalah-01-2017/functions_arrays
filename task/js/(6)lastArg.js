49221fc91658427b3159aca9643081b3f66e7b21
Denis <denis_golovin@mail.ua>
2017-02-05 19:50:44 +0200

    Update (6)lastArg.js

--- task/js/(6)lastArg.js
+++ task/js/(6)lastArg.js
@@ -1,9 +1,4 @@
 function lastArg() {
-	var last;
-	var nul = null;
-	var empty = [];
-	return last = arguments === empty ? nul : arguments[arguments.length - 1];
+	return arguments.length === 0 ? [] : arguments[arguments.length - 1];
 }
-
 lastArg(1,2,3,4,5);
-lastArg(); // ???
